cf3cba659d480f387a645342e1996a6984c459eb
Woojin KIM <woog2nius@gmail.com>
2020-04-02 10:20:25 +0900

    fix time

--- index.ts
+++ index.ts
@@ -35,7 +35,7 @@ async function postFridayMeetingForThisWeek() {
   );
 }
 
-cron.schedule('20 10 * * 1-4', postDailyMeetingForTmr);
+cron.schedule('0 12 * * 1-4', postDailyMeetingForTmr);
 cron.schedule('0 18 * * 5', postMondayMeetingForNextWeek);
 cron.schedule('0 13 * * 5', postFridayMeetingForThisWeek);
 
